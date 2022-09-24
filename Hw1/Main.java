0f026957b49628a080127793d3615afbbd752c3c
Steven J Boucher <Steven.boucher1@marist.edu>
2022-09-24 03:43:38 -0400

    now its got the right name

--- Hw1/Main.java
+++ Hw1/Main.java
@@ -1,18 +1,29 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class Filereader {
+public class Main {
+
+
   public static String main(String[] args) throws Exception {
     String magicItem = "";
     // pass the path to the file as a parameter
     File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw1\\magicitems.txt");
     Scanner sc = new Scanner(file);
-
+    
+    
     while (sc.hasNextLine()) {
       magicItem = sc.nextLine().toUpperCase();
-
+      Arraify(magicItem);
     }
     return magicItem;
+
+  }
+  public static  void Arraify(String magicItem) {
+    int itemLength = magicItem.length();
+    for (int i = 0; i < itemLength; i++) {
+      char[] arrayedItem = magicItem.toCharArray();
+      System.out.println(arrayedItem);
+    }
 
   }
   /* 
@@ -20,12 +31,5 @@ public class Filereader {
     arrayedItem.length;
   }
 */
-  public void Arraify(String magicItem) {
-    int itemLength = magicItem.length();
-    for (int i = 0; i < itemLength; i++) {
-      char[] arrayedItem = magicItem.toCharArray();
-      System.out.println(arrayedItem);
-    }
-   
-  }
+
 }
