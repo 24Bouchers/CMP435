6a37fafd67ce9e123fff5af96f4e75855dc1b7bd
Steven J Boucher <Steven.boucher1@marist.edu>
2022-09-24 03:43:02 -0400

    needed this name for another file

--- Hw1/Main.java
+++
@@ -1,8 +0,0 @@
-//Swapped to Java, Can't Get C++ to run on this computer, I tried Vs and Clion and neither would opperate
-//Will talk about it during lab report 
-
-//import magicitems.txt;
-public class Main {
-
-    }
-  
