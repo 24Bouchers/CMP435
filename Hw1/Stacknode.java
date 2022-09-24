b75593f23c2fa70d2984d395af866a13a1c77d40
Steven J Boucher <Steven.boucher1@marist.edu>
2022-09-23 22:03:28 -0400

    Working(Kinda)Update

--- Hw1/Stacknode.java
+++ Hw1/Stacknode.java
@@ -1,7 +1,7 @@
 public class Stacknode {    
   //Represent a node of the singly linked list    
   class Node{    
-      String data;    
+    String data;    
       Node next;    
           
       public Node(String data) {    
@@ -10,46 +10,50 @@ public class Stacknode {
       }    
   }    
    
- //This is the head
-  public Node head = null;   
-//Makes a new nodee
-  public void addNode(String data) {
-    Node newNode = new Node(data);    
-  
+  //Start of the Node  
+  public Node head = null;    
+ //this is the part actually making it
+  public void addNode(String data) {    
+      //Create a new node    
+      Node newNode = new Node(data);    
+          
 //if the stack is empty, make it not empty 
-    if (head == null){
-      head = newNode;
-    }
-  }
-  public void display(){
-    Node current = head;
-  
-  if (head == null){
-    System.out.println("List is Empty");
-    return;
-  }
+      if(head == null) {    
 
-  System.out.println("Nodes of singly linked list: ");    
-  while(current != null) {    
-      //Prints each node by incrementing pointer    
-      System.out.print(current.data + " ");    
-      current = current.next;    
-  }    
-  System.out.println();    
-  }    
+          head = newNode;     
+      }    
 
-public static void main(String[] args) {    
+  }    
       
-  Stacknode sList = new Stacknode();    
+  //display() will display all the nodes present in the list    
+  public void display() {    
+      //Node current will point to head    
+      Node current = head;    
+          
+      if(head == null) {    
+          System.out.println("List is empty");    
+          return;    
+      }    
+      System.out.println("Nodes of singly linked list: ");    
+      while(current != null) {    
+          //Prints each node by incrementing pointer    
+          System.out.print(current.data + " ");    
+          current = current.next;    
+      }    
+      System.out.println();    
+  }    
       
-  //Add nodes to the list    
-  sList.addNode("test1");    
-  sList.addNode("test2");    
-  sList.addNode("test3");    
-  sList.addNode("test4");    
-      
-  //Displays the nodes present in the list    
-  sList.display();    
+  public static void main(String[] args) {    
+          
+    Stacknode sList = new Stacknode();    
+          
+  //This will become letters for magic items hopefully within the next hour  
+      sList.addNode("1");    
+      sList.addNode("2");    
+      sList.addNode("3");    
+      sList.addNode("4");    
+          
+      //Displays the nodes present in the list    
+      sList.display();    
+  }    
 }    
-}
-  
