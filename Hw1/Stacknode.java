0f7b7aa166b06686d9f396d81b54a40b93069278
Steven J Boucher <Steven.boucher1@marist.edu>
2022-09-23 21:14:46 -0400

    New Stacknode setup

--- Hw1/Stacknode.java
+++ Hw1/Stacknode.java
@@ -1,18 +1,16 @@
 public class Stacknode {
-//Data Types
-    public String letter;
-    private Stacknode next;
-//Constructor    
-    public Stacknode(String data) {
-      this.letter = data;
-      this.next = null;
+
+  class node{
+    String data;
+    Stacknode next;
+    
+  public node(String data){
+    this.data = data;
+    this.next = null; 
     }
- //makes a new node 
-    public void setNextNode(Stacknode node) {
-      this.next = node;
-    }
- //obtain information on the next node 
-    public Stacknode getNextNode() {
-      return this.next;
     }
   }
+
+
+    
+  
