6f86d44ffcfc6342f25529a63d2ed78c78a6b1bb
Steven J Boucher <Steven.boucher1@marist.edu>
2022-10-06 17:52:57 -0400

    Pre Array Test

--- Hw2/Main.java
+++ Hw2/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) throws Exception {
     int fileSize = 0;
+    String unmagicalArray[];
     // pass the path to the file as a parameter
     File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw2\\magicitems.txt");
     Scanner sc = new Scanner(file);
@@ -13,5 +14,14 @@ public class Main {
         fileSize++;
       }
       System.out.println(fileSize);
+    //set the unmagicalArray size to the size of the file
+      unmagicalArray = new String[fileSize];  // allocating memory to array
+      //Fill that array
+      while(sc.hasNextLine()) {
+        int i = 0;
+        String item = sc.nextLine();
+        unmagicalArray[i] = item;
+        i++;
+      }
     }
   }
