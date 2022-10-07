3b13cf1766227a58dbe9eca04af00db87809dfa0
Steven J Boucher <Steven.boucher1@marist.edu>
2022-10-07 00:03:10 -0400

    FileReader satisfactory

--- Hw2/Main.java
+++ Hw2/Main.java
@@ -1,36 +1,43 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import javax.swing.plaf.basic.BasicComboBoxUI.ItemHandler;
-
 public class Main {
-  public static void main(String[] args) throws Exception {
+
+  public static void main(String[] args) throws FileNotFoundException {
+    FileReader();
+  }
+
+  public static String[] FileReader() throws FileNotFoundException {
+  //Var Declerations
     int fileSize = 0;
-    String unmagicalArray[];
-    String item = "test";
+    String magicalArray[];
+    String item = "";
     int c = 0;
-    int co = 1;
     // pass the path to the file as a parameter
     File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw2\\magicitems.txt");
     Scanner sc0 = new Scanner(file);
-
-      while(sc0.hasNextLine()) {
-        sc0.nextLine();
-        fileSize++;
-      }
-      sc0.close();
-    //set the unmagicalArray size to the size of the file
-      unmagicalArray = new String[fileSize];  // allocating memory to array
-      //Fill that array
-    Scanner sc1 = new Scanner(file);
-      while(sc1.hasNextLine()) {
-
-        item = sc1.nextLine();
-        
-        unmagicalArray[c] = item;
-        System.out.println(unmagicalArray[c]);
-        c++;
-      }
-      sc1.close();
+    //Find the Ammount of Lines in the file
+    while (sc0.hasNextLine()) {
+      sc0.nextLine();
+      fileSize++;
     }
+    sc0.close();
+    // set the unmagicalArray size to the size of the file
+    magicalArray = new String[fileSize]; 
+    // Fill that array
+    Scanner sc1 = new Scanner(file);
+    while (sc1.hasNextLine()) {
+      item = sc1.nextLine();
+      item = item.toUpperCase();
+      magicalArray[c] = item;
+      System.out.println(magicalArray[c]);
+      c++;
+    }
+    sc1.close();
+    return magicalArray;
   }
+
+  public static void SelectionSort() throws FileNotFoundException {
+  }
+}
