e123bc52eb13964ffea228482336727e44f1dbde
Steven J Boucher <Steven.boucher1@marist.edu>
2022-10-07 17:44:56 -0400

    ITS ALIVE

--- Hw2/Main.java
+++ Hw2/Main.java
@@ -4,12 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 
-
-
   public static void main(String[] args) throws FileNotFoundException {
-    
-    
-  //Var Declerations
+
+    // Var Declerations
     int fileSize = 0;
     String magicalArray[];
     String item = "";
@@ -17,19 +14,18 @@ public class Main {
     // pass the path to the file as a parameter
     File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw2\\magicitems.txt");
     Scanner sc0 = new Scanner(file);
-    //Find the Ammount of Lines in the file
+    // Find the Ammount of Lines in the file
     while (sc0.hasNextLine()) {
       sc0.nextLine();
       fileSize++;
     }
     sc0.close();
     // set the unmagicalArray size to the size of the file
-    magicalArray = new String[fileSize]; 
+    magicalArray = new String[fileSize];
     // Fill that array
     Scanner sc1 = new Scanner(file);
     while (sc1.hasNextLine()) {
       item = sc1.nextLine();
-      item = item.toUpperCase();
       magicalArray[c] = item;
 
       c++;
@@ -40,30 +36,30 @@ public class Main {
 
   public static void InsertionSort(String[] magicalArray) throws FileNotFoundException {
 
-
-  
     int comparisons = 0;
     String temp[];
     temp = new String[2];
     temp[1] = null;
-    for(int i = 0; i < magicalArray.length; i++){
-      int check = 0;
-      if((i+1) > magicalArray.length){
-        i = magicalArray.length +1;
+    for (int a = 0; a < magicalArray.length; a++) {
+      for (int i = 0; i < magicalArray.length; i++) {
+        String compareOne = magicalArray[i].toString();
+        for (int j = 0; j < magicalArray.length; j++) {
+          String compareTwo = magicalArray[j].toString();
+          int check = compareOne.compareToIgnoreCase(compareTwo);
+          comparisons++;
+          if (check < 0) {
+            temp[1] = magicalArray[i];
+            magicalArray[i] = magicalArray[j];
+            magicalArray[j] = temp[1];
+          }
+        }
       }
-      String compareOne = magicalArray[i].toString();
-      String compareTwo = magicalArray[i+1].toString();
-
-
-      check = compareOne.compareTo(compareTwo);
-      comparisons ++;
-      if(check > 0){
-        temp[1] = magicalArray[i];
-        magicalArray[i] = magicalArray[i+1];
-        magicalArray[i+1] = temp[1];
-        temp[1] = null;
-      }
-      System.out.println(magicalArray[i]);
     }
+    for (int i = 0; i < magicalArray.length; i++) {
+      System.out.print(magicalArray[i]);
+      System.out.print(" ");
+      System.out.print("|");
+    }
+    System.out.print(comparisons);
   }
 }
