dd1c220b21506bffc678e48b46952603e3e73521
Steven J Boucher <Steven.boucher1@marist.edu>
2022-11-05 03:10:09 -0400

    Final Commit

--- Hw3/Main.java
+++ Hw3/Main.java
@@ -2,16 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 public class Main {
 
   public static void main(String[] args) throws FileNotFoundException {
-    // FUNCTIONS GOES HERE
     String[] magicalArray = FileReader();
     LinearSearch(magicalArray);
     BinarySearch(magicalArray);
     hashSearch(hashyTime(magicalArray), magicalArray);
-
   }
 
   public static void LinearSearch(String[] magicalArray) {
@@ -20,14 +17,14 @@ public class Main {
     int NUMSEARCHES = 42;
     // This is set up for how many items were locating
     for (int i = 0; i < NUMSEARCHES; i++) {
-      int random_int = (int) Math.floor(Math.random() * (666-1));
+      int random_int = (int) Math.floor(Math.random() * (666 - 1));
       String magicItem = magicalArray[random_int];
       // Counter Counts the ammount of comparisons each item makes
       int counter = 0;
       boolean found = false;
       // Looks for the item
       while (found != true) {
-        
+
         if (magicItem == magicalArray[counter]) {
           found = true;
           total = total + counter;
@@ -46,7 +43,7 @@ public class Main {
     // This is set up for how many items were locating
     for (int i = 0; i < NUMSEARCHES; i++) {
       // Variable Decleration
-      int random_int = (int) Math.floor(Math.random() * (666-1));
+      int random_int = (int) Math.floor(Math.random() * (666 - 1));
       String item = magicalArray[random_int];
       int counter = 0;
       int lp = 0;
@@ -65,43 +62,70 @@ public class Main {
         }
         total = total + counter;
       }
-      
+
     }
     int avgDecimal = total % NUMSEARCHES;
     int avgWhole = total / NUMSEARCHES;
     System.out.println("Binary Search took a average of " + avgWhole + "." + avgDecimal + " Comparisons");
 
   }
-  
-  public static LinkedList hashyTime(String[] magicalArray){
+
+  public static LinkedList hashyTime(String[] magicalArray) {
     LinkedList magicList = new LinkedList();
-    for(int i = 0; i < magicalArray.length; i++){
+    for (int i = 0; i < magicalArray.length; i++) {
       magicList.insert(Hashing.makeHashCode(magicalArray[i]));
     }
     return magicList;
   }
 
-  public static void hashSearch(LinkedList magicList, String[] magicalArray){
+  public static void hashSearch(LinkedList magicList, String[] magicalArray) {
     int selectedForSearch = 0;
     int total = 0;
     int NUMSEARCHES = 42;
 
-    //Selecting Search item
-    for(int i = 0; i < NUMSEARCHES; i ++){
+    // Selecting Search item
+    for (int i = 0; i < NUMSEARCHES; i++) {
       int count = 0;
       selectedForSearch = (i * 2);
       String item = magicalArray[selectedForSearch];
       int specialItem = Hashing.makeHashCode(item);
-      if(specialItem == Hashing.makeHashCode(magicalArray[selectedForSearch])){
-        count ++;
-        }
-        total = total + count;
+      if (specialItem == Hashing.makeHashCode(magicalArray[selectedForSearch])) {
+        count++;
       }
-      int avgDecimal = total % NUMSEARCHES;
-      int avgWhole = total / NUMSEARCHES;
-      System.out.println("Hash Search took a average of " + avgWhole + "." + avgDecimal + " Comparisons");
+      total = total + count;
     }
-  
+    int avgDecimal = total % NUMSEARCHES;
+    int avgWhole = total / NUMSEARCHES;
+    System.out.println("Hash Search took a average of " + avgWhole + "." + avgDecimal + " Comparisons");
+  }
+
+  public static String[] FileReader() throws FileNotFoundException {
+    int fileSize = 0;
+    String magicalArray[];
+    String item = "";
+    int c = 0;
+    // pass the path to the file as a parameter
+    File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw2\\magicitems.txt");
+    Scanner sc0 = new Scanner(file);
+    // Find the Ammount of Lines in the file
+    while (sc0.hasNextLine()) {
+      sc0.nextLine();
+      fileSize++;
+    }
+    sc0.close();
+    magicalArray = new String[fileSize];
+    Scanner sc1 = new Scanner(file);
+    // Array Filler
+    while (sc1.hasNextLine()) {
+      item = sc1.nextLine();
+      magicalArray[c] = item;
+      c++;
+    }
+    sc1.close();
+    MergeSort(magicalArray);
+    return magicalArray;
+  }
+
   public static void MergeSort(String[] magicalArray) throws FileNotFoundException {
     int size = magicalArray.length;
     if (size < 2) {
@@ -154,31 +178,5 @@ public class Main {
       k++;
     }
   }
-  public static String[] FileReader() throws FileNotFoundException{
-    int fileSize = 0;
-    String magicalArray[];
-    String item = "";
-    int c = 0;
-    // pass the path to the file as a parameter
-    File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw2\\magicitems.txt");
-    Scanner sc0 = new Scanner(file);
-    // Find the Ammount of Lines in the file
-    while (sc0.hasNextLine()) {
-      sc0.nextLine();
-      fileSize++;
-    }
-    sc0.close();
-    magicalArray = new String[fileSize];
-    Scanner sc1 = new Scanner(file);
-    // Array Filler
-    while (sc1.hasNextLine()) {
-      item = sc1.nextLine();
-      magicalArray[c] = item;
-      c++;
-    }
-    sc1.close();
-    MergeSort(magicalArray);
-    return magicalArray;
-  }
-}
 
+}
