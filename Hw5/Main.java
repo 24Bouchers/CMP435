0462caf0c54a316ab3f8a34506e809fc35be0c67
Steven J Boucher <Steven.boucher1@marist.edu>
2022-12-05 12:30:19 -0500

    Run Clean Up

---
+++ Hw5/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+
+    void Part1() {
+
+    }
+    public static void FileReader() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Owner\\Documents\\GitHub\\CMP435\\Hw2\\magicitems.txt");
+        Scanner sc0 = new Scanner(file);
+        // Find the Ammount of Lines in the file
+
+        while (sc0.hasNextLine()) {
+
+        }
+        sc0.close();
+        
+      }
+}
