24d59abcd792be11deee2ff086104d04f22d709d
Steven J Boucher <Steven.boucher1@marist.edu>
2022-12-09 19:15:10 -0500

    Cleanup

--- Hw5/Main.java
+++ Hw5/Main.java
@@ -36,7 +36,6 @@ public class Main {
             int totalWeight = 0;
             char weight1 = ' ';
             char weight2 = ' ';
-            char weight3 = ' ';
             String weightString = "";
             textCommand = sc1.nextLine();
             if (textCommand != "") {
@@ -51,71 +50,68 @@ public class Main {
                     graph = new Graph();
                     sc1.nextLine();
                 }
-                // Checksto see if we are adding a vertex
-                if (textCommand.charAt(4) == 'v') {
-                    commandLength = textCommand.length();
-                    if (commandLength == 13) {
-                        firstDigit = textCommand.charAt(11);
-                        secondDigit = textCommand.charAt(12);
-                        numString = String.valueOf(firstDigit) + String.valueOf(secondDigit);
-                        numInt = Integer.parseInt(numString);
-                        graph.insert(numInt);
-                    } else {
-                        firstDigit = textCommand.charAt(11);
-                        numString = String.valueOf(firstDigit);
-                        numInt = Integer.parseInt(numString);
-                        graph.insert(numInt);
+                if (textCommand.charAt(0) != '-' && textCommand.charAt(0) != 'n') {
+                    // Checksto see if we are adding a vertex
+                    if (textCommand.charAt(4) == 'v') {
+                        commandLength = textCommand.length();
+                        if (commandLength == 13) {
+                            firstDigit = textCommand.charAt(11);
+                            secondDigit = textCommand.charAt(12);
+                            numString = String.valueOf(firstDigit) + String.valueOf(secondDigit);
+                            numInt = Integer.parseInt(numString);
+                            graph.insert(numInt);
+                        } else {
+                            firstDigit = textCommand.charAt(11);
+                            numString = String.valueOf(firstDigit);
+                            numInt = Integer.parseInt(numString);
+                            graph.insert(numInt);
+                        }
+                        System.out.println("Added Node: " + numInt);
                     }
-                    System.out.println("Added Node: " + numInt);
-                }
-                // Checks To See if we are adding and Edge
-                if (textCommand.charAt(4) == 'e') {
-                    System.out.print(textCommand.charAt(0));
-                    commandLength = textCommand.length();
-                    int offset = 0;
-                    // Setup For the Vertex Selection
-                    // Where are we starting from
-                    Start1 = textCommand.charAt(9);
-                    if (textCommand.charAt(10) != ' ') {
-                        Start2 = textCommand.charAt(10);
-                        startingVertexString = String.valueOf(Start1) + String.valueOf(Start2);
-                        offset++;
-                    } else {
-                        startingVertexString = String.valueOf(Start1);
-                    }
-                    startVertex = Integer.parseInt(startingVertexString);
-                    // Where we going to
-                    to1 = textCommand.charAt(13 + offset);
-                    if (textCommand.charAt(14 + offset) != ' ') {
-                        to2 = textCommand.charAt(14);
-                        toVertexString = String.valueOf(to1) + String.valueOf(to2);
-                        offset++;
-                    } else {
-                        toVertexString = String.valueOf(to1);
-                    }
-                    toVertex = Integer.parseInt(toVertexString);
+                    // Checks To See if we are adding and Edge
+                    if (textCommand.charAt(4) == 'e') {
+                        commandLength = textCommand.length();
+                        int offset = 0;
+                        // Setup For the Vertex Selection
+                        // Where are we starting from
+                        Start1 = textCommand.charAt(9);
+                        if (textCommand.charAt(10) != ' ') {
+                            Start2 = textCommand.charAt(10);
+                            startingVertexString = String.valueOf(Start1) + String.valueOf(Start2);
+                            offset++;
+                        } else {
+                            startingVertexString = String.valueOf(Start1);
+                        }
+                        startVertex = Integer.parseInt(startingVertexString);
+                        // Where we going to
+                        to1 = textCommand.charAt(13 + offset);
+                        if (textCommand.charAt(14 + offset) != ' ') {
+                            to2 = textCommand.charAt(14);
+                            toVertexString = String.valueOf(to1) + String.valueOf(to2);
+                            offset++;
+                        } else {
+                            toVertexString = String.valueOf(to1);
+                        }
+                        toVertex = Integer.parseInt(toVertexString);
 
-                    // What is the Weight of the Path
-                    weight1 = textCommand.charAt(15 + offset);
-                    weight2 = textCommand.charAt(16 + offset);
+                        // What is the Weight of the Path
+                        weight1 = textCommand.charAt(commandLength -2);
+                        weight2 = textCommand.charAt(commandLength -1);
 
-                    if (weight1 == ' ') {
-                        weightString = String.valueOf(weight2);
+                        if (weight1 == ' ') {
+                            weightString = String.valueOf(weight2);
+                        }
+                        if (weight2 == ' ') {
+                            weightString = String.valueOf(weight1);
+                        }
+                        if (weight1 != ' ' && weight2 != ' ') {
+                            weightString = String.valueOf(weight1) + String.valueOf(weight2);
+                        }
+                        System.out.print(textCommand + ": ");
+                        System.out.println(weightString);
+                        totalWeight = Integer.parseInt(weightString);
+                        graph.edge(startVertex, toVertex, totalWeight);
                     }
-                    if (weight2 == ' ') {
-                        weightString = String.valueOf(weight1);
-                    }
-                    if (weight1 != ' ' && weight2 != ' ') {
-                        weightString = String.valueOf(weight1) + String.valueOf(weight2);
-                    }
-                    System.out.print(textCommand + ": ");
-                    System.out.println(weightString);
-                    totalWeight = Integer.parseInt(weightString);
-                    graph.edge(startVertex, toVertex, totalWeight);
-                }
-
-                if (textCommand == " ") {
-                    graph.show();
                 }
             }
         }
